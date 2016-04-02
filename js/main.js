636c30a0df9eb9b6955e080f998d4de90aabb8ea
Bastian König <ba.koe96@gmail.com>
2016-04-03 01:07:57 +0200

    Switched main.js indentation from tabs to spaces

--- js/main.js
+++ js/main.js
@@ -4,23 +4,23 @@ function getURLParameter(name) {
 }
 
 $(document).ready(function() {
-	L.mapbox.accessToken = 'pk.eyJ1IjoiaGFuZGNvZGVkIiwiYSI6ImNpbWoxN2VzdDAwMGt2dW00aHVvOTNnZXMifQ.qlhvpqfP_H8e6E8hxCoCdw';
+    L.mapbox.accessToken = 'pk.eyJ1IjoiaGFuZGNvZGVkIiwiYSI6ImNpbWoxN2VzdDAwMGt2dW00aHVvOTNnZXMifQ.qlhvpqfP_H8e6E8hxCoCdw';
 
-	var geojson = [];
-	var productid = getURLParameter('productid');
+    var geojson = [];
+    var productid = getURLParameter('productid');
 
-	$.getJSON('data.json', function(data) {
-		for (var i = 0; i < data.length; i++) {
-			var id = data[i].id;
-			if (id == productid) {
-				geojson = data[i].geojson;
-			}
-		}
-	}).done(function() {
-		var map = L.mapbox.map('map', 'mapbox.streets')
-	    .setView([52.5055829, 13.3932478], 3)
-		.featureLayer.setGeoJSON(geojson);
+    $.getJSON('data.json', function(data) {
+        for (var i = 0; i < data.length; i++) {
+            var id = data[i].id;
+            if (id == productid) {
+                geojson = data[i].geojson;
+            }
+        }
+    }).done(function() {
+        var map = L.mapbox.map('map', 'mapbox.streets')
+        .setView([52.5055829, 13.3932478], 3)
+        .featureLayer.setGeoJSON(geojson);
 
-		L.mapbox.styleLayer('mapbox://styles/handcoded/cimj399zn00a9cem33b5cubif').addTo(map);
-	});
+        L.mapbox.styleLayer('mapbox://styles/handcoded/cimj399zn00a9cem33b5cubif').addTo(map);
+    });
 });
