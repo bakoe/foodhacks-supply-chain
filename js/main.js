67df0b863259b47a9c58c7b2ff04799a956cd491
Bastian König <ba.koe96@gmail.com>
2016-04-03 03:43:06 +0200

    Fixed indentation

--- js/main.js
+++ js/main.js
@@ -34,34 +34,34 @@ $(document).ready(function() {
 
     L.mapbox.accessToken = 'pk.eyJ1IjoiaGFuZGNvZGVkIiwiYSI6ImNpbWoxN2VzdDAwMGt2dW00aHVvOTNnZXMifQ.qlhvpqfP_H8e6E8hxCoCdw';
 
-	var geojson = [];
-	var productId = getURLParameter('productId');
+    var geojson = [];
+    var productId = getURLParameter('productId');
 
-	$.getJSON('data.json', function(data) {
-		for (var i = 0; i < data.length; i++) {
-			if (data[i].id == productId) {
-				geojson = data[i].geojson;
-			}
-		}
-	}).done(function() {
-		var map = L.mapbox.map('map', 'mapbox.streets')
-			.setView([52.5055829, 13.3932478], 4);
+    $.getJSON('data.json', function(data) {
+        for (var i = 0; i < data.length; i++) {
+            if (data[i].id == productId) {
+                geojson = data[i].geojson;
+            }
+        }
+    }).done(function() {
+        var map = L.mapbox.map('map', 'mapbox.streets')
+            .setView([52.5055829, 13.3932478], 4);
 
-		var myLayer = L.mapbox.featureLayer();
+        var myLayer = L.mapbox.featureLayer();
 
-		myLayer.setGeoJSON(geojson)
-			.addTo(map);
+        myLayer.setGeoJSON(geojson)
+            .addTo(map);
 
-		var polyline_options = {
-	    	color: '#000000'
-		};
+        var polyline_options = {
+            color: '#000000'
+        };
 
-		var polyline = L.polyline([], polyline_options).addTo(map);
+        var polyline = L.polyline([], polyline_options).addTo(map);
 
-		myLayer.eachLayer(function(layer) {
-			polyline.addLatLng(layer.getLatLng());
-		});
+        myLayer.eachLayer(function(layer) {
+            polyline.addLatLng(layer.getLatLng());
+        });
 
-		// L.mapbox.styleLayer('mapbox://styles/mapbox/emerald-v8').addTo(map);
-	});
+        // L.mapbox.styleLayer('mapbox://styles/mapbox/emerald-v8').addTo(map);
+    });
 });
